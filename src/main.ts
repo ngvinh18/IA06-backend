39274fff885f48d671dfde5ce2794ac6d2e19311
ngvinh18 <thanhvinh18122003@gmail.com>
2025-12-03 16:18:38 +0700

    push nhanh

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,12 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as dotenv from 'dotenv';
 
+dotenv.config();
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.enableCors();
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
