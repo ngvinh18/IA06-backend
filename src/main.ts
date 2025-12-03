f6b53f5b048e2052d8513b21869d30302512337b
ngvinh18 <thanhvinh18122003@gmail.com>
2025-12-03 19:37:32 +0700

    puysh nhanh

--- src/main.ts
+++ src/main.ts
@@ -2,23 +2,27 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as dotenv from 'dotenv';
+import * as express from 'express';
 
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+
   // CORS cho frontend Render & local dev
   app.enableCors({
     origin: [
-      "https://ia06-frontend-y9g3.onrender.com", // frontend deployed
-      "http://localhost:5173",                   // local React dev
+      "https://ia06-frontend-y9g3.onrender.com",
+      "http://localhost:5173",
     ],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
   });
 
-  // Validation
+  // Validation global
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -27,9 +31,7 @@ async function bootstrap() {
     }),
   );
 
-  // Render cung cấp PORT qua biến môi trường PORT
   const port = process.env.PORT || 3000;
-
   await app.listen(port, () => {
     console.log(`🚀 Server running on port ${port}`);
   });
